45fcbc97b2d1de3736b09df5207a6cc5f9af260f
Tien <tien04100@gmail.com>
2023-10-12 11:11:33 +0700

    fix categoryId api

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -68,6 +68,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
     return NextResponse.json(category);
   } catch (error) {
